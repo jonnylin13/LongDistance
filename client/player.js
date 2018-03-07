da3e664fb4c2300b343be2eea84cd3040eb30c2b
jonnylin13 <jonnylin13@gmail.com>
2018-03-06 23:02:20 -0800

    test

--- client/player.js
+++ client/player.js
@@ -142,11 +142,10 @@ function msg_listener(req, sender, send_response) {
                 if (is_loaded()) {
                     
                     clearInterval(load);
-                    /** 
-                    var video = get_video();
+                    /**var video = get_video();
                     video.currentTime = req.progress.elapsed;
                     if (req.player_state == PLAYER_STATE.Play) video.paused = false;
-                    else video.paused = true**/
+                    else video.paused = true;**/
                     send_response({type: 'player_update_ack'});
                 }
             }, 500);
@@ -156,13 +155,13 @@ function msg_listener(req, sender, send_response) {
                     progress: get_progress()
                 });
         } else if (req.type === 'timeout') {
-            get_video().paused = true;
+            /**get_video().paused = true;
             setTimeout(function() {
                 get_video().paused = false;
             }, 5000);
             send_response({
                 type: 'timeout_ack'
-            });
+            });**/
         }
     } 
 }
