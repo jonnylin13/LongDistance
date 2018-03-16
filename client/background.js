08805cba5d05ada69e9cf11f985d9a1367a66cc5
jonnylin13 <jonnylin13@gmail.com>
2018-03-15 23:55:35 -0700

    hmm

--- client/background.js
+++ client/background.js
@@ -133,6 +133,7 @@ function update_listener(event) {
 
                             var listener = function (tab_id, change_info, tab) {
                                 console.log('test');
+                                console.log(change_info.status);
                                 if (!change_info.status || change_info.status != 'complete') return;
                                 if (tab_id == tabs[0].id) {
                                     full_player_update(tabs[0].id, controller);
