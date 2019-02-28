a851de578469c729ce8338372d5d65bfe0817801
jonnylin13 <jonnylin13@gmail.com>
2019-02-28 13:24:41 -0800

    #29 done, needs testing

--- extension/src/socket.js
+++ extension/src/socket.js
@@ -1,98 +1,108 @@
-const Constants = require('./util/constants');
+const Constants = require("./util/constants");
 
 class SocketController {
-    constructor(tabListener) {
-        this._tabs = tabListener;
-        this._ws = null;
-        if (!this._connect()) {
-            throw new Error('<LDN> Could not connect to WebSocket server!');
-        }
-    }
+	constructor(tabListener) {
+		this._tabs = tabListener;
+		this._ws = null;
+		if (!this._connect()) {
+			throw new Error("<LDN> Could not connect to WebSocket server!");
+		}
+	}
 
-    _connect() {
-        if (!this._socketOpen()) {
-            try {
-                this._ws = new WebSocket(Constants.WS_URL);
-                this._ws.onopen = () => {
-                    this._ws.onmessage = (msg) => {
-                         this._handleMessage(msg);
-                    };
-                }
+	// ===============
+	// Handler Methods
+	// ===============
 
-                this._ws.close = (event) => {
-                    this._onClose();
-                };
-                return true;
-            } catch (e) {
-                console.log(e);
-                return false;
-            } 
-        }
-        return true;
-    }
+	_onClose(event) {
+		// What is this for really?
+		if (this._tabs.clientState.connectedToLobby()) {
+			this._connect();
+		}
+	}
 
-    _onClose(event) {
-        // What is this for really?
-        if (this._tabs.clientState.connectedToLobby()) {
-            this._connect();
-        }
-    }
+	// Inbound Socket Messages
+	_handleMessage(msg) {
+		const data = JSON.parse(msg.data);
 
-    _handleMessage(msg) {
-        const data = JSON.parse(msg.data);
+		console.log("<LDN> Socket received msg of type: ", data.type);
 
+		this._handleRequest(data);
+		this._handleResponse(data);
+	}
 
-        console.log('<LDN> Socket received msg of type: ', data.type);
+	_handleResponse(data) {
+		if (data.type === "create_lobby_ack") {
+			const lobbyId = data.lobby_id;
+			this._tabs.clientState.lobbyId = lobbyId;
+			this._tabs.clientState.sessionId = data.session_id;
 
-        this._handleRequest(data);
-        this._handleResponse(data);
-    }
+			// Send a start_lobby_ack to popup.js
+			chrome.tabs.sendMessage(this._tabs.tabId, {
+				type: "start_lobby_ack",
+				lobbyId: lobbyId,
+				code: 1
+			});
+		}
+	}
 
-    _handleResponse(data) {
+	_handleRequest(data) {
+		// Todo
+	}
 
-        if (data.type === 'create_lobby_ack') {
-            
-            const lobbyId = data.lobby_id;
-            this._tabs.clientState.lobbyId = lobbyId;
-            this._tabs.clientState.sessionId = data.session_id;
+	// ===============
+	// Private Methods
+	// ===============
 
-            // Send a start_lobby_ack to popup.js
-            chrome.tabs.sendMessage(this._tabs.tabId, {
-                'type': 'start_lobby_ack',
-                'lobbyId': lobbyId,
-                'code': 1
-            });
-        }
-    }
+	_connect() {
+		if (!this._socketOpen()) {
+			try {
+				this._ws = new WebSocket(Constants.WS_URL);
+				this._ws.onopen = () => {
+					this._ws.onmessage = msg => {
+						this._handleMessage(msg);
+					};
+				};
 
-    _handleRequest(data) {
-        // Todo
-    }
+				this._ws.close = event => {
+					this._onClose();
+				};
+				return true;
+			} catch (e) {
+				console.log(e);
+				return false;
+			}
+		}
+		return true;
+	}
 
-    _socketOpen() {
-        return this._ws !== null && this._ws.readyState === 4;
-    }
+	_socketOpen() {
+		return this._ws !== null && this._ws.readyState === 4;
+	}
 
-    _send(json) {
-        if (this._connect()) {
-            this._ws.send(JSON.stringify(json));
-        } else {
-            // Todo: handle error
-            return;
-        }
-    }
+	_send(json) {
+		if (this._connect()) {
+			this._ws.send(JSON.stringify(json));
+		} else {
+			// Todo: handle error
+			return;
+		}
+	}
 
-    createLobby() {
-        if (this._tabs.clientState.sessionId === '') {
-            console.log('<LDN> Tried to start a lobby with no session ID!');
-            return;
-        }
-        this._send({'type': 'create_lobby', 'session_id': this._tabs.clientState.sessionId});
-    }
+	// ==============
+	// Public Methods
+	// ==============
 
-    get tabListener() {
-        return this._tabs;
-    }
+	// Outbound Socket Messages
+	createLobby() {
+		this._send({
+			type: "create_lobby"
+		});
+	}
+
+	// Setters and Getters
+	get tabListener() {
+		return this._tabs;
+	}
 }
 
 module.exports = SocketController;
--- server/src/services/lobby-service.js
+++ server/src/services/lobby-service.js
@@ -1,91 +1,79 @@
-const shortid = require('shortid');
-const Lobby = require('../models/lobby');
-const User = require('../models/user');
+const shortid = require("shortid");
+const Lobby = require("../models/lobby");
+const User = require("../models/user");
 
 class LobbyService {
+	constructor() {
+		this._userMap = {}; // sessionId: lobbyId
+		this._lobbies = {}; // lobbyId: lobby
+	}
 
-    constructor() {
-        this._userMap = {}; // sessionId: lobbyId
-        this._lobbies = {}; // lobbyId: lobby
-    }
+	removeLobby(lobby) {
+		delete this._lobbies[lobby.id];
+		if (lobby.emitTask) {
+			lobby.stopEmitTask();
+		}
+	}
 
-    removeLobby(lobby) {
-        
-        delete this._lobbies[lobby.id];
-        if (lobby.emitTask) {
-            lobby.stopEmitTask();
-        }
-    }
+	createLobby(socket, sessionId) {
+		// Todo: Check if user is in lobby
+		try {
+			const lobbyId = shortid.generate();
+			const user = new User(sessionId, socket);
+			const lobby = new Lobby(lobbyId, user);
+			// Todo
+			this._userMap[sessionId] = lobbyId;
+			this._lobbies[lobbyId] = lobby;
+			return true;
+		} catch (e) {
+			return false;
+		}
+	}
 
-    createLobby(socket, sessionId) {
-        
-        // Todo: Check if user is in lobby
-        try {
-            const lobbyId = shortid.generate();
-            const user = new User(sessionId, socket);
-            const lobby = new Lobby(lobbyId, user);
-            // Todo
-            this._userMap[sessionId] = lobbyId;
-            this._lobbies[lobbyId] = lobby;
-            return true;
+	connectLobby(socket, lobby, sessionId) {
+		try {
+			// Check if user is in lobby
+			if (this.hasLobby(sessionId)) {
+				// Get our lobby record
+				const _lobby = this._lobbies[this._userMap[sessionId]];
+				_lobby.removeUser(sessionId);
 
-        } catch(e) {
-            return false;
-        }
+				// Is this check necessary?
+				if (_lobby.master === null) {
+					delete this._lobbies[this._userMap[sessionId]];
+					delete this._userMap[sessionId];
+				}
+			}
 
-    }
+			const user = new User(sessionId, socket);
+			lobby.addSlave(user);
+			this._userMap[sessionId] = lobby.id;
+			return true;
+		} catch (e) {
+			return false;
+		}
+	}
 
-    connectLobby(socket, lobby, sessionId) {
+	hasLobby(sessionId) {
+		return this._userMap[sessionId] !== "";
+	}
 
-        try {
-            // Check if user is in lobby
-            if (this.hasLobby(sessionId)) {
-                
-                // Get our lobby record
-                const _lobby = this._lobbies[this._userMap[sessionId]];
-                _lobby.removeUser(sessionId);
-                if (_lobby.master === null) {
-                    delete this._lobbies[this._userMap[sessionId]];
-                    this._userMap[sessionId] = '';
-                }
+	getLobbyId(sessionId) {
+		return this._userMap[sessionId];
+	}
 
-            }
+	// Todo: unused
+	getLobby(lobbyId) {
+		return this._lobbies[lobbyId];
+	}
 
-            const user = new User(sessionId, socket);
-            lobby.addSlave(user);
-            this._userMap[sessionId] = lobby.id;
-            return true;
-        } catch(e) {
-            return false;
-        }
-        
-    }
+	getLobbyFromSession(sessionId) {
+		return this._lobbies[this._userMap[sessionId]];
+	}
 
-    hasLobby(sessionId) {
-        return this._userMap[sessionId] !== '';
-    }
-
-    initUser(sessionId) {
-        this._userMap[sessionId] = '';
-    }
-
-    getLobbyId(sessionId) {
-        return this._userMap[sessionId];
-    }
-
-    // Todo: unused
-    getLobby(lobbyId) {
-        return this._lobbies[lobbyId];
-    }
-
-    getLobbyFromSession(sessionId) {
-        return this._lobbies[this._userMap[sessionId]];
-    }
-
-    isConnected(sessionId) {
-        return sessionId in this._userMap;
-    }
-
+	isConnected(sessionId) {
+		return sessionId in this._userMap;
+	}
 }
 
 module.exports = LobbyService;
