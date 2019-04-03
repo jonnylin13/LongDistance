054b29e348d833ee65334b7129fd3641ae1305b3
jonnylin13 <jonnylin13@gmail.com>
2019-04-03 13:58:56 -0700

    work on #30

--- extension/src/background.js
+++ extension/src/background.js
@@ -2,156 +2,158 @@ const SocketController = require('./socket');
 const ClientState = require('./models/client-state');
 
 class BackgroundResponse {
-
-    static validateFields(data, send) {
-        // Todo: Implement
-    }
+  static validateFields(data, send) {
+    // Todo: Implement
+  }
 }
- 
 
 class Background {
+  constructor() {
+    console.log('<LDN> Starting client extension');
+    this._clientState = new ClientState();
+    this._tabs = new BackgroundTabListener(this._clientState);
+    this._socketController = new SocketController(this._tabs);
+    chrome.runtime.onMessage.addListener((msg, sender, send) => {
+      this._onMessage(msg, sender, send);
+    });
+    console.log('<LDN> Background script started!');
+  }
 
-    constructor() {
-        console.log('<LDN> Starting client extension');
-        this._clientState = new ClientState();
-        this._tabs = new BackgroundTabListener(this._clientState);
-        this._socketController = new SocketController(this._tabs);
-        chrome.runtime.onMessage.addListener((msg, sender, send) => {
-            this._onMessage(msg, sender, send);
-        });
-        console.log('<LDN> Background script started!');
+  _onMessage(data, sender, send) {
+    if (!data || !data.type) {
+      console.log(
+        '<LDN> Background script received broken message from a content script.'
+      );
+      return;
     }
+    const payload = {
+      type: data.type + '_ack'
+    };
 
-    _onMessage(data, sender, send) {
-
-        if (!data || !data.type) {
-            console.log('<LDN> Background script received broken message from a content script.');
-            return;
-        }
-        const payload = {
-            'type': data.type + '_ack'
-        };
-
-        console.log('<LDN> Background script received message of type: ', data.type);
-        if (data.type === 'popup_loaded') {
-
-        } else if (data.type === 'get_lobby_id') {
-
-        } else if (data.type === 'start_lobby') {
-            this._socketController.createLobby();
-            // This one sends a special response
-        }
-        console.log('<LDN> Background script sending response to content script: ');
-        console.log(payload);
-        send(payload);
+    console.log(
+      '<LDN> Background script received message of type: ',
+      data.type
+    );
+    if (data.type === 'popup_loaded') {
+    } else if (data.type === 'get_lobby_id') {
+    } else if (data.type === 'start_lobby') {
+      this._socketController.createLobby();
+      // This one sends a special response
+    } else if (data.type === 'disconnect') {
+      // TODO
+      // Send a disconnect to server?
     }
-
-    
-
+    console.log('<LDN> Background script sending response to content script: ');
+    console.log(payload);
+    send(payload);
+  }
 }
 
 class BackgroundTabListener {
+  constructor(clientState) {
+    this._clientState = clientState;
 
-    constructor(clientState) {
+    this._uncacheTab();
+    this._queryNetflixTab();
 
-        this._clientState = clientState;
+    chrome.tabs.onCreated.addListener(tab => {
+      this._onCreated(tab);
+    });
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+      this._onUpdated(tabId, changeInfo, tab);
+    });
+    chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+      this._onRemoved(tabId, removeInfo);
+    });
+  }
 
-        this._uncacheTab();
-        this._queryNetflixTab();
-
-        chrome.tabs.onCreated.addListener((tab) => { this._onCreated(tab) });
-        chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => { this._onUpdated(tabId, changeInfo, tab) });
-        chrome.tabs.onRemoved.addListener((tabId, removeInfo) => { this._onRemoved(tabId, removeInfo) });
-
+  _onCreated(tab) {
+    console.log('<LDN> Created: ', tab.url);
+    if (BackgroundTabListener.isNetflix(tab) && !this._isTabCached()) {
+      this._startNetflixScript(tab.id);
     }
+  }
 
-    _onCreated(tab) {
-        console.log('<LDN> Created: ', tab.url);
-        if (BackgroundTabListener.isNetflix(tab) && !this._isTabCached()) {
-            this._startNetflixScript(tab.id);
+  _onUpdated(tabId, changeInfo, tab) {
+    // Tracks URL params
+    if (tab.url === this._clientState.urlParams) return;
+    if (!changeInfo.status || changeInfo.status !== 'complete') return;
+
+    console.log('<LDN> Updated tab: ', tab.url);
+    if (BackgroundTabListener.isNetflix(tab)) {
+      if (!this._isTabCached()) {
+        this._startNetflixScript(tab.id);
+      }
+      const urlParams = BackgroundTabListener.getUrlParams(tab);
+      if (this._clientState.urlParams !== urlParams) {
+        this._clientState.urlParams = urlParams;
+      }
+    }
+  }
+
+  _onRemoved(tabId, removeInfo) {
+    if (this._tabId === tabId) {
+      this._uncacheTab();
+      console.log('<LDN> Removed :(');
+    }
+  }
+
+  _queryNetflixTab() {
+    chrome.tabs.query({ title: 'Netflix' }, tabs => {
+      if (tabs.length === 0) this._uncacheTab();
+      else {
+        if (!this._isTabCached()) {
+          // Todo: Fix this
+          this._startNetflixScript(tabs[0].id);
         }
-    }
+      }
+    });
+  }
 
-    _onUpdated(tabId, changeInfo, tab) {
-        // Tracks URL params
-        if (tab.url === this._clientState.urlParams) return;
-        if (!changeInfo.status || changeInfo.status !== 'complete') return;
-        
-        console.log('<LDN> Updated tab: ', tab.url);
-        if (BackgroundTabListener.isNetflix(tab)) {
-            if (!this._isTabCached()) {
-                this._startNetflixScript(tab.id);
-            }
-            const urlParams = BackgroundTabListener.getUrlParams(tab);
-            if (this._clientState.urlParams !== urlParams) {
-                this._clientState.urlParams = urlParams;
-            }
+  _startNetflixScript(tabId) {
+    this._tabId = tabId;
+    console.log('<LDN> Starting Netflix script...');
+    chrome.tabs.executeScript(
+      tabId,
+      {
+        file: 'netflix.bundle.js',
+        runAt: 'document_idle'
+      },
+      err => {
+        if (!err || err[0]) {
+          console.log('<LDN> Controller started!');
+          chrome.pageAction.show(tabId, undefined);
+        } else {
+          console.log('<LDN> Failed to start netflix.bundle.js!');
         }
-    }
+      }
+    );
+  }
 
-    _onRemoved(tabId, removeInfo) {
-        if (this._tabId === tabId) {
-            this._uncacheTab();
-            console.log('<LDN> Removed :(');
-        }
-    }
+  _uncacheTab() {
+    this._tabId = -1;
+  }
 
-    _queryNetflixTab() {
-        chrome.tabs.query(
-            {title: 'Netflix'},
-            (tabs) => {
-                if (tabs.length === 0) this._uncacheTab();
-                else {
-                    if (!this._isTabCached()) {
-                        // Todo: Fix this
-                        this._startNetflixScript(tabs[0].id);
-                    }
-                }
-            }
-        );
-    }
+  _isTabCached() {
+    return this._tabId !== -1;
+  }
 
-    _startNetflixScript(tabId) {
-        this._tabId = tabId;
-        console.log('<LDN> Starting Netflix script...');
-        chrome.tabs.executeScript(tabId, {
-            file: 'netflix.bundle.js',
-            runAt: 'document_idle'
-        }, (err) => {
-            if (!err || err[0]) {
-                console.log('<LDN> Controller started!');
-                chrome.pageAction.show(tabId, undefined);
-            } else {
-                console.log('<LDN> Failed to start netflix.bundle.js!');
-            }
-        });
-    }
+  get tabId() {
+    return this._tabId;
+  }
 
-    _uncacheTab() {
-        this._tabId = -1;
-    }
+  get clientState() {
+    return this._clientState;
+  }
 
-    _isTabCached () {
-        return this._tabId !== -1;
-    }
+  static isNetflix(tab) {
+    return tab.url.includes('netflix.com/');
+  }
 
-    get tabId() {
-        return this._tabId;
-    }
-
-    get clientState() {
-        return this._clientState;
-    }
-
-    static isNetflix(tab) {
-        return tab.url.includes('netflix.com/');
-    }
-
-    static getUrlParams(tab) {
-        if (!tab) return '';
-        return tab.url.split('netflix.com/')[1].split('?')[0];
-    }
-
+  static getUrlParams(tab) {
+    if (!tab) return '';
+    return tab.url.split('netflix.com/')[1].split('?')[0];
+  }
 }
 
 new Background();
--- server/src/server.js
+++ server/src/server.js
@@ -1,158 +1,159 @@
-const WebSocket = require("ws");
-const crypto = require("crypto");
+const WebSocket = require('ws');
+const crypto = require('crypto');
 
-const LobbyService = require("./services/lobby-service");
-const LDNResponse = require("./util/response");
+const LobbyService = require('./services/lobby-service');
+const LDNResponse = require('./util/response');
 
 class LDNServer {
-	constructor(start = true, port = 3000) {
-		this._port = port;
-		this._lobbyService = new LobbyService();
-		if (start) this.start();
+  constructor(start = true, port = 3000) {
+    this._port = port;
+    this._lobbyService = new LobbyService();
+    if (start) this.start();
 
-		process.on("SIGINT", () => {
-			if (this._server) {
-				this._server.close();
-			}
-			console.log("<Info> Shutting down");
-			process.exit();
-		});
-	}
+    process.on('SIGINT', () => {
+      if (this._server) {
+        this._server.close();
+      }
+      console.log('<Info> Shutting down');
+      process.exit();
+    });
+  }
 
-	// ===============
-	// Private Methods
-	// ===============
-	_start() {
-		this._server = new WebSocket.Server({ port: this._port });
-		console.log("<Info> Listening on port: " + this.port);
-		this._server.on("connection", (socket, req) => {
-			this._onConnection(socket, req);
-		});
-	}
+  // ===============
+  // Private Methods
+  // ===============
+  _start() {
+    this._server = new WebSocket.Server({ port: this._port });
+    console.log('<Info> Listening on port: ' + this.port);
+    this._server.on('connection', (socket, req) => {
+      this._onConnection(socket, req);
+    });
+  }
 
-	_generateSessionId() {
-		const sessionId = crypto.randomBytes(16).toString("hex");
-		return sessionId;
-	}
+  _generateSessionId() {
+    const sessionId = crypto.randomBytes(16).toString('hex');
+    return sessionId;
+  }
 
-	// ===============
-	// Handler Methods
-	// ===============
-	_onConnection(socket, req) {
-		console.log(
-			"<Info> Connection received from: ",
-			req.connection.remoteAddress
-		);
+  // ===============
+  // Handler Methods
+  // ===============
+  _onConnection(socket, req) {
+    console.log(
+      '<Info> Connection received from: ',
+      req.connection.remoteAddress
+    );
 
-		socket.on("message", msg => {
-			this._onMessage(socket, msg);
-		});
-	}
+    socket.on('message', msg => {
+      this._onMessage(socket, msg);
+    });
+  }
 
-	_onMessage(socket, msg) {
-		const data = JSON.parse(msg);
-		if (!data) {
-			console.log(
-				"<Error> Received bad data from socket connection: ",
-				req.connection.remoteAddress
-			);
-			return;
-		}
-		if (!LDNResponse.validateFields(socket, data, ["type"])) return;
-		console.log("<Info> Received a message: ");
-		console.log(msg);
-		this._handleRequest(socket, data);
-		this._handleResponse(data);
-	}
+  _onMessage(socket, msg) {
+    const data = JSON.parse(msg);
+    if (!data) {
+      console.log(
+        '<Error> Received bad data from socket connection: ',
+        req.connection.remoteAddress
+      );
+      return;
+    }
+    if (!LDNResponse.validateFields(socket, data, ['type'])) return;
+    console.log('<Info> Received a message: ');
+    console.log(msg);
+    this._handleRequest(socket, data);
+    this._handleResponse(data);
+  }
 
-	// Inbound Socket Requests
-	_handleRequest(socket, data) {
-		const payload = { type: data.type + "_ack" };
+  // Inbound Socket Requests
+  _handleRequest(socket, data) {
+    const payload = { type: data.type + '_ack' };
 
-		if (data.type === "create_lobby") {
-			const sessionId = _generateSessionId();
+    if (data.type === 'create_lobby') {
+      const sessionId = _generateSessionId();
 
-			if (this._lobbyService.createLobby(socket, sessionId)) {
-				payload.code = 1;
-				payload.lobby_id = this._lobbyService.getLobbyId(sessionId);
-				payload.session_id = sessionId;
-			} else {
-				payload.code = 0;
-				payload.msg = "Calling createLobby() failed.";
-			}
-		} else if (data.type === "connect_lobby") {
-			if (!LDNResponse.validateFields(socket, data, ["lobby_id"])) return;
+      if (this._lobbyService.createLobby(socket, sessionId)) {
+        payload.code = 1;
+        payload.lobby_id = this._lobbyService.getLobbyId(sessionId);
+        payload.session_id = sessionId;
+      } else {
+        payload.code = 0;
+        payload.msg = 'Calling createLobby() failed.';
+      }
+    } else if (data.type === 'connect_lobby') {
+      if (!LDNResponse.validateFields(socket, data, ['lobby_id'])) return;
 
-			const sessionId = _generateSessionId();
-			const lobbyId = data.lobby_id;
-			const lobby = this._lobbyService.getLobby(lobbyId);
+      const sessionId = _generateSessionId();
+      const lobbyId = data.lobby_id;
+      const lobby = this._lobbyService.getLobby(lobbyId);
 
-			if (!LDNResponse.validateLobby(socket, lobby, data)) return;
+      if (!LDNResponse.validateLobby(socket, lobby, data)) return;
 
-			if (this._lobbyService.connectLobby(socket, lobby, sessionId)) {
-				payload.code = 1;
-				payload.lobby_id = lobbyId;
-				payload.session_id = sessionId;
-			} else {
-				payload.code = 0;
-				payload.msg = "Calling connectLobby() failed.";
-			}
-		} else if (data.type === "start_control") {
-			if (!LDNResponse.validateFields(socket, data, ["session_id"]))
-				return;
+      if (this._lobbyService.connectLobby(socket, lobby, sessionId)) {
+        payload.code = 1;
+        payload.lobby_id = lobbyId;
+        payload.session_id = sessionId;
+      } else {
+        payload.code = 0;
+        payload.msg = 'Calling connectLobby() failed.';
+      }
+    } else if (data.type === 'start_control') {
+      if (!LDNResponse.validateFields(socket, data, ['session_id'])) return;
 
-			const sessionId = data.sessionId;
-			const lobby = this._lobbyService.getLobbyFromSession(sessionId);
-			if (!LDNResponse.validateLobby(socket, lobby, data)) return;
-			// Todo: Authorize session id
+      const sessionId = data.sessionId;
+      const lobby = this._lobbyService.getLobbyFromSession(sessionId);
+      if (!LDNResponse.validateLobby(socket, lobby, data)) return;
+      // Todo: Authorize session id
 
-			if (lobby.emitTask) {
-				// Todo: Test/never reached
-				lobby.stopEmitTask();
-			}
+      if (lobby.emitTask) {
+        // Todo: Test/never reached
+        lobby.stopEmitTask();
+      }
 
-			if (lobby.startEmitTask()) {
-				payload.code = 1;
-			}
-		}
-		socket.send(JSON.stringify(payload));
-		return;
-	}
+      if (lobby.startEmitTask()) {
+        payload.code = 1;
+      }
+    } else if (data.type === 'disconnect') {
+      // TODO
+    }
+    socket.send(JSON.stringify(payload));
+    return;
+  }
 
-	_handleResponse(data) {
-		// These do not send a response, they just update server state
-		if (data.type === "update_ack") {
-			// Todo: Handle server state update
-		}
-	}
+  _handleResponse(data) {
+    // These do not send a response, they just update server state
+    if (data.type === 'update_ack') {
+      // Todo: Handle server state update
+    }
+  }
 
-	// ==============
-	// Public Methods
-	// ==============
+  // ==============
+  // Public Methods
+  // ==============
 
-	start() {
-		if (this._server) {
-			this._server.close(err => {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				this._start();
-			});
-		} else {
-			this._start();
-		}
-	}
+  start() {
+    if (this._server) {
+      this._server.close(err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        this._start();
+      });
+    } else {
+      this._start();
+    }
+  }
 
-	get port() {
-		return this._port;
-	}
+  get port() {
+    return this._port;
+  }
 
-	close() {
-		this._server.close(err => {
-			console.log(err);
-		});
-	}
+  close() {
+    this._server.close(err => {
+      console.log(err);
+    });
+  }
 }
 
 module.exports = LDNServer;
